6228dabed7fba0e3b5a1c69178f0318061f199ee
Nathan Mathew <nathanmathewv@gmail.com>
2025-12-11 12:21:29 +0530

    frontend clean

--- frontend/gen-proto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p src/proto
-protoc -I=../backend/services/proto-common/src/main/proto \
-  --plugin=protoc-gen-ts=./node_modules/@protobuf-ts/plugin/bin/protoc-gen-ts \
-  --ts_out=src/proto \
-  --ts_opt=service_type=grpc-web \
-  ../backend/services/proto-common/src/main/proto/*.proto
